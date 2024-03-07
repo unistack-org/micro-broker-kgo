ec7a22b2dc89174b5b20c7c43f2c972cd31cefd7
Vasiliy Tolstov <v.tolstov@unistack.org>
2024-03-07 09:06:33 +0300

    fix double init
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- kgo.go
+++ kgo.go
@@ -54,6 +54,7 @@ var DefaultRetryBackoffFn = func() func(int) time.Duration {
 }()
 
 type Broker struct {
+	init      bool
 	c         *kgo.Client
 	kopts     []kgo.Opt
 	connected bool
@@ -182,6 +183,10 @@ func (k *Broker) Init(opts ...broker.Option) error {
 	k.Lock()
 	defer k.Unlock()
 
+	if len(opts) == 0 && k.init {
+		return nil
+	}
+
 	for _, o := range opts {
 		o(&k.opts)
 	}
@@ -204,6 +209,8 @@ func (k *Broker) Init(opts ...broker.Option) error {
 			k.kopts = append(k.kopts, v...)
 		}
 	}
+
+	k.init = true
 
 	return nil
 }
